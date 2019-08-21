932df137daa1c33ce045200ff4f9ea34acf9acfb
Karel Škrabal <k.skrabal@nexnet.cz>
2019-08-21 20:00:27 +0200

    adding migrations file

--- SudokuUI/Migrations/Configuration.cs
+++ SudokuUI/Migrations/Configuration.cs
@@ -10,7 +10,7 @@ namespace SudokuUI.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(SudokuUI.PuzzleContext context)
